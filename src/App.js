ad562ce6c14296fc0941531cd9538e871f37f9e4
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-11 16:43:34 -0300

    props ++

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <UserCard title='hola'/>
-      <UserCard x="bye"/>
-      <UserCard y={30}/>
-      <UserCard z={true}/>
-      <UserCard a={[1, 2, 3]}/>
+      <UserCard title="hola mundo"/>
+      <UserCard title="hola react"/>
+      <UserCard title="hola jSx"/>
+      <UserCard title="hola hola js"/>
+      <UserCard title="hola hola marte"/>
+      <UserCard title="hola usuario"
+      nombre="Marta"
+      saldo="45.25610"
+      />
+      <UserCard nombre="Gabriel"/>
 
   
     </div>
--- src/sumador.jsx
+++ src/sumador.jsx
@@ -1,7 +1,7 @@
 
 export function UserCard(props){
     console.log(props);
-    return <h1> User Card </h1>
+    return <h1> {props.title} {props.nombre} su saldo es: {props.saldo}</h1>
 }
 
 
