a9a8958b0649b71315ef732c07d633fb7a9afc91
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-11 10:34:08 -0300

    sumador component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Sumador from './sumador';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <Sumador/>
     </div>
   );
 }
---
+++ src/sumador.js
@@ -0,0 +1,8 @@
+function Sumador (){
+    function add(x,y) {
+        return x + y;
+    }
+    return <h1> {add(40,50)} </h1>
+}
+
+export default Sumador;
