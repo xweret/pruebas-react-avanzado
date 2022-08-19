d77ff2778110e72470f8b7159adcf71a81c1d787
Gabriel Garofalo <gabriel695681@gmail.com>
2022-08-19 10:39:25 -0300

    props destructuring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { UserCard } from "./sumador";
-import "./App.css";
+import UserCard from './sumador';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <UserCard
-        name="Ryan"
-        saldo={32000}
-        married={true}
-        points={[99, 33.3, 22, 2]}
-        adress={{
-          street: "123 main street",
-          city: "New York",
-        }}
+      <UserCard name='Ryan' saldo={32000} married={false}
+      points={[99, 33.3, 22,2]}
+      addres={{
+        street: '123 main street',
+        city: 'New York'
+      }
+      }
       />
     </div>
   );
--- src/sumador.jsx
+++ src/sumador.jsx
@@ -1,8 +1,21 @@
-
-export function UserCard({title,name,saldo = 0}){
-    console.log(title);
-    return <h1> {title} {name} su saldo es: {saldo}</h1>
+function UserCard({name, saldo, married, addres}) {
+  console.log(name);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>Su Saldo es de {saldo}💵</p>
+      <p>{married ? "is married" : "is single"}</p>
+      <ul>
+        <li>
+            City: {addres.city}
+        </li>
+        <li>
+            Street: {addres.street}
+        </li>
+    </ul>
+    </div>
+   
+  );
 }
 
-
-
+export default UserCard;
